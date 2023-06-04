0a747afdf38ceef27d5a2c980049092bc9be0d16
Amin Borhani <105095446+Philipjakes@users.noreply.github.com>
2023-06-04 18:48:08 +0330

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,10 +1,9 @@
-# import module1 as md
-# print(md.diction)
+import module1 as md
+print(md.diction)
 # output:{'company_name': 'techArt', 'computer_users': {'paterson', 'edward', 'kate'}, 'login_password': 'TECHART39402@'}
-# print(md.diction["computer_users"])
+print(md.diction["computer_users"])
 # output:{'kate', 'edward', 'paterson'}
 
-# IMPORT FROM MODULE:
 from module1 import diction
 print(diction["login_password"])
 # OR:
