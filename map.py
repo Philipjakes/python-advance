a9db64dda375a3bf45de58995801dc73ec1afcd4
Amin Borhani <105095446+Philipjakes@users.noreply.github.com>
2023-06-04 18:47:20 +0330

    Update map.py

--- map.py
+++ map.py
@@ -1,16 +1,16 @@
-# map:executes a specified function for each item in an iterable and return a specified item.
-# def myf(object):
-#     return len(object)
-# newlist = ["moon","sun","planet"]
-# print(list(map(myf,newlist)))
+map:executes a specified function for each item in an iterable and return a specified item.
+def myf(object):
+    return len(object)
+newlist = ["moon","sun","planet"]
+print(list(map(myf,newlist)))
 # # output:[4, 3, 6]
 
-# def myf(object):
-#     return object[1]
-# newlist = ["moon","sun","planet"]
-# print(list(map(myf,newlist)))
+def myf(object):
+    return object[1]
+newlist = ["moon","sun","planet"]
+print(list(map(myf,newlist)))
 # # output:[o, u, l]
 
-# newlist = ["moon","sun","planet"]
-# print(list(map(lambda x:x[1],newlist)))
+newlist = ["moon","sun","planet"]
+print(list(map(lambda x:x[1],newlist)))
 # # output:[o, u, l]
